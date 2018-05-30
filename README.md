cc36e431edf05fbe828ba0c317f8fd773682b538
Paul Varache <paul@kano.me>
2018-05-30 14:43:06 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# kwc-loading-animation
+
+Polymer wrapper around @kano/loading-animation
+
+## Development
+
+Install dependencies
+```
+yarn
+```
+
+Serve the files
+```
+polymer serve --npm --compile always
+```
---
+++ kwc-loading-animation.js
@@ -0,0 +1,65 @@
+import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { LoadingAnimation } from '@kano/loading-animation/index.js';
+
+class KWCAnimationLoader extends PolymerElement {
+    static get is() { return 'kano-app-loader'; }
+    static get template() {
+        return html`
+            <style>
+                :host {
+                    display: block;
+                }
+            </style>
+        `;
+    }
+    static get properties() {
+        return {
+            pipSize: {
+                type: Number,
+                value: 9,
+                observer: '_inject'
+            },
+            noAutoStart: {
+                type: Boolean,
+                value: false,
+            }
+        };
+    }
+    connectedCallback() {
+        super.connectedCallback();
+    }
+    _inject() {
+        if (this._loader) {
+            this._loader.stop();
+            this._loader.delete();
+        }
+        this._loader = new LoadingAnimation(this.shadowRoot, { pipSize: this.pipSize });
+        if (!this.noAutoStart) {
+            this._loader.start();
+        }
+    }
+    start () {
+        if (!this._loader) {
+            return;
+        }
+        this._loader.start();
+    }
+    stop () {
+        if (!this._loader) {
+            return;
+        }
+        this._loader.stop();
+    }
+    disconnectedCallback() {
+        super.disconnectedCallback();
+        if (!this._loader) {
+            return
+        }
+        this._loader.stop();
+        this._loader.delete();
+        this._loader = null;
+    }
+}
+
+customElements.define(KWCAnimationLoader.is, KWCAnimationLoader);
